d810c7ca0b7b83325542da0f837abaf366fc71fe
Sascha Eisenmann <sascha.eisenmann@pragtics.de>
2023-03-17 14:58:19 +0100

    Add UnderConstruction view for unfinished sites

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import { Stack } from '../components/shared/stack/Stack'
 import { StackDivider } from '../components/shared/stack/StackDivider'
 import { profile } from '../data/profile'
 import { responsiveValue } from '../utils/ResponsiveUtils'
+import { UnderConstruction } from '../components/under-construction/UnderConstruction'
 
 type DataProps = {
   files: {
@@ -36,6 +37,10 @@ type DataProps = {
 
 const IndexPage = ({ data }: PageProps<DataProps>) => {
   const [toolsFilter] = useQueryParamString('tools', '')
+
+  if (profile.useUnderConstructionPage) {
+    return <UnderConstruction />
+  }
 
   const normalizedForFilter = (input: string) =>
     input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
