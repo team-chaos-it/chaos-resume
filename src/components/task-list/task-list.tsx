31bca195e7b36de0cafdd0fa62a2940157b7dc6c
slemling <64414883+slemling@users.noreply.github.com>
2023-04-23 17:34:49 +0200

    feat: create TaskList which fades tasks after adjustable amount of tasks for screen

---
+++ src/components/task-list/task-list.tsx
@@ -0,0 +1,31 @@
+import { Task } from './task'
+
+type TaskListProps = { tasks: string[]; hideAfterLines?: number }
+
+export const TaskList = ({
+  tasks,
+  hideAfterLines,
+  ...props
+}: TaskListProps) => {
+  return (
+    <ul {...props}>
+      {tasks.map((value, index) => (
+        <Task
+          css={{
+            '@media screen': {
+              display: hideAfterLines
+                ? index >= hideAfterLines
+                  ? 'none'
+                  : 'block,'
+                : 'block',
+            },
+          }}
+          fade={hideAfterLines ? index === hideAfterLines - 1 : false}
+          key={value}
+        >
+          {value}
+        </Task>
+      ))}
+    </ul>
+  )
+}
