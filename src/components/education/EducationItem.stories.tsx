5d765f5bed4d7a759a0413b26612be91e3053192
slemling <64414883+slemling@users.noreply.github.com>
2023-05-07 18:50:19 +0200

    feat(education-item): make date optional

--- src/components/education/EducationItem.stories.tsx
+++ src/components/education/EducationItem.stories.tsx
@@ -1,6 +1,6 @@
 import * as icons from '@fortawesome/free-solid-svg-icons'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { EducationItem } from './EducationItem'
+import { ComponentMeta, Story } from '@storybook/react'
+import { EducationItem, EducationItemProps } from './EducationItem'
 
 export default {
   component: EducationItem,
@@ -14,15 +14,36 @@ export default {
       options: Object.keys(icons),
       mapping: icons,
     },
+    elements: {
+      control: {
+        type: 'check',
+        options: ['from', 'to'],
+      },
+    },
   },
 } as ComponentMeta<typeof EducationItem>
 
-const Template: ComponentStory<typeof EducationItem> = (args) => (
-  <EducationItem {...args} />
+const Template: Story<EducationItemProps & { elements: ['from', 'to'] }> = ({
+  elements,
+  title,
+  subtitle,
+  from,
+  to,
+}) => (
+  <EducationItem
+    title={title}
+    subtitle={subtitle}
+    from={elements.includes('from') ? from : undefined}
+    to={elements.includes('to') ? to : undefined}
+  />
 )
 
 export const Default = Template.bind({})
 Default.storyName = 'EducationItem'
 Default.args = {
-  children: 'Lorem ipsum dolor sit amet',
+  title: 'Master of Disaster',
+  subtitle: 'Chaos University',
+  from: '2012',
+  to: '2015',
+  elements: ['from', 'to'],
 }
